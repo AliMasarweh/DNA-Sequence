cd81e9e336a723afd5d8840992894f2367ad75a1
Ali Masarwe <alis.masarwe@gmail.com>
2021-01-11 14:27:34 +0200

    Started class codonand added it's source to the lib

--- header/codon.h
+++ header/codon.h
@@ -5,4 +5,30 @@
 #ifndef DNA_SEQUENCE_CODON_H
 #define DNA_SEQUENCE_CODON_H
 
+#include <string>
+#include <vector>
+
+#include "nucleotide.h"
+
+class Codon {
+public:
+    const static unsigned char s_codonSize = 4;
+    Codon(char codon[Codon::s_codonSize]);
+    Codon(std::string codon);
+    Codon(const Codon& codon);
+
+    Codon& operator=(char codon[Codon::s_codonSize]);
+    Codon& operator=(std::string codon);
+    Codon& operator=(const Codon& codon);
+
+    Codon pair() const;
+
+    Nucleotide& operator[](size_t index);
+
+    static bool isStartingCodon(const Codon& codon);
+    static bool isEndingCodon(const Codon& codon);
+private:
+    std::vector<Nucleotide> m_codonSeq;
+};
+
 #endif //DNA_SEQUENCE_CODON_H
--- src/codon.cpp
+++ src/codon.cpp
@@ -2,3 +2,10 @@
 // Created by ali-masa on 1/10/21.
 //
 
+#include "../header/codon.h"
+
+Codon::Codon(char codon[Codon::s_codonSize]) {
+    for (unsigned  char i = 0; i < s_codonSize; ++i)
+        m_codonSeq.push_back(Nucleotide(codon[i]));
+}
+
