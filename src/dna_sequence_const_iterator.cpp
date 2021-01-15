73b67a272c136d9c2e354ad042fd06773a6df317
root <alis.masarwe@gmail.com>
2021-01-15 19:52:25 +0200

    Implemented slice method

--- src/dna_sequence_const_iterator.cpp
+++ src/dna_sequence_const_iterator.cpp
@@ -85,3 +85,30 @@ bool operator==(const CIterator& it1, const CIterator& it2) {
 bool operator!=(const CIterator& it1, const CIterator& it2) {
     return !(it1 == it2);
 }
+
+
+DNASequence DNASequence::slice(size_t start, size_t end) {
+    char seq[end - start];
+    for (size_t i = start; i < end - 1; ++i) {
+        seq[i-start] = m_sequence[i].asCharacter();
+    }
+    seq[end - start - 1] = 0;
+
+    return DNASequence(seq);
+}
+
+DNASequence DNASequence::pairSequence() const {
+    return DNASequence(nullptr);
+}
+
+DNASequence::iterator DNASequence::find(DNASequence subSequence) const {
+    return DNASequence::iterator();
+}
+
+size_t DNASequence::count(DNASequence subSequence) const {
+    return 0;
+}
+
+std::vector<iterator> DNASequence::findAll(DNASequence subSequence) const {
+    return std::vector<iterator>();
+}
